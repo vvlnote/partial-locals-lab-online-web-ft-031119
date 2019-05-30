4afca8f3c779eaa1f152f6f156026958fa79d91f
Joy Lin <vvlnote@hotmail.com>
2019-05-30 15:51:44 +0000

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,11 @@
 class ClassroomsController < ApplicationController
+
   def show
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
   end
 
   def index
     @classrooms = Classroom.all
   end
+
 end
